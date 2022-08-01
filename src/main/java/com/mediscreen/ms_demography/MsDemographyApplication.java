8937807369fc898b81c32a7212ac3209d2296807
Marie <marie@exemple.fr>
2022-08-01 18:10:46 +0200

    Add Swagger for documentation

--- src/main/java/com/mediscreen/ms_demography/MsDemographyApplication.java
+++ src/main/java/com/mediscreen/ms_demography/MsDemographyApplication.java
@@ -2,9 +2,10 @@ package com.mediscreen.ms_demography;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MsDemographyApplication {
 
 	public static void main(String[] args) {

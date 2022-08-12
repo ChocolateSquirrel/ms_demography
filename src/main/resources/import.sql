9d7cd008af6ca9103e28490803db7935b329671b
Marie <marie@exemple.fr>
2022-08-12 16:33:56 +0200

    Add data for sprint3

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,14 @@ INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('
 INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Test','TestBorderline','1945-06-24','MALE', '2 High St', '200-333-4444');
 INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Test','TestInDanger','2004-06-18','MALE', '3 Club Road', '300-444-5555');
 INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Test','TestEarlyOnset','2002-06-28','FEMALE', '4 Valley Dr', '400-555-6666');
+
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Lucas', 'Ferguson', '1968-06-22','MALE', '2 Warren Street', '387-866-1399');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Pippa', 'Rees', '1952-09-27','FEMALE', '745 West Valley Farms Drive', '628-423-0993');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Edward', 'Arnold', '1952-11-11','MALE', '599 East Garden Ave', '123-727-2779');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Anthony', 'Sharp', '1946-11-26','MALE', '894 Hall Street', '451-761-8383');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Wendy', 'Ince', '1958-06-29','FEMALE', '4 Southampton Road', '802-911-9975');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'),'Tracey', 'Ross', '1949-12-07','FEMALE', '40 Sulphur Springs Dr', '131-396-5049');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'), 'Claire', 'Wilson', '1966-12-31','FEMALE', '12 Cobblestone St', '300-452-1091');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'), 'Max', 'Buckland', '1945-06-24','MALE', '193 Vale St', '833-534-0864');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'), 'Natalie', 'Clark', '1964-06-18','FEMALE', '12 Beechwood Road', '241-467-9197');
+INSERT INTO patient(id, prenom, nom, dob, sex, address, phone) VALUES (NEXTVAL('patient_id_seq'), 'Piers', 'Bailey', '1959-06-28','MALE', '1202 Bumble Dr', '747-815-0557');
